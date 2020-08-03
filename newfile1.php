2e36a515f1957451f08bbdec8d3de7d70cb993d0
GraceOrji <gracious_treasure@yahoo.com>
2020-08-03 22:12:49 +0100

    commiting newfile1

---
+++ newfile1.php
@@ -0,0 +1,13 @@
+<?php echo "Hello Git" ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Newbranch1</title>
+</head>
+
+<body>
+
+<h1>Testing a new file1 in newbranch </h1>
+</body>
+</html>
